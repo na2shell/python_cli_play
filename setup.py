a0504fa4b645f9b85e310d2e20c49ed3542d56d9
na2shell <nagafeltf7@gmail.com>
2022-01-15 18:47:11 +0900

    initial

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="samplecli",
+    version="0.0.1",
+    description="A small package",
+    author="hgoe_hoge_bar",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    entry_points={
+        "console_scripts": [
+            "sample_hoge=sample_hoge.core:main",
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ]
+)
